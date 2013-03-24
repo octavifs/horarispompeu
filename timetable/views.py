973614d587a4b2f07441aa1f16bcf25f7f5ab78c
Octavi Font Sola <tavifont@gmail.com>
2013-03-24 20:40:33 +0100

    Changed the views, so it shows a list of the parsed subjects.

--- timetable/views.py
+++ timetable/views.py
@@ -2,14 +2,14 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.template import Context, loader
 
-from timetable.models import Class
+from timetable.models import Subject
 
 
 # Create your views here.
 def index(request):
-    classes_list = Class.objects.all()
+    subject_list = Subject.objects.all()
     template = loader.get_template('index.html')
     context = Context({
-        'classes_list': classes_list
+        'subject_list': subject_list
     })
     return HttpResponse(template.render(context))

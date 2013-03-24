40df3a276f684cca071a2eedb3f8f4a01a8fe1fc
Octavi Font Sola <tavifont@gmail.com>
2013-03-24 02:04:42 +0100

    Added all models to the django admin panel
    Added SubjectAlias model (links a mispelled subject with the right tuple).
    
    Solved some issues with the Calendar model (wrong inheritance + wrong usage of FileField).

--- timetable/admin.py
+++ timetable/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from timetable.models import Class
+from timetable.models import *
 
+# Register all classes from the model to the admin panel
+admin.site.register(Faculty)
+admin.site.register(Degree)
+admin.site.register(Subject)
+admin.site.register(SubjectAlias)
+admin.site.register(Year)
 admin.site.register(Class)
+admin.site.register(DegreeSubject)
+admin.site.register(Calendar)
--- timetable/models.py
+++ timetable/models.py
@@ -26,6 +26,19 @@ class Subject(models.Model):
     name = models.CharField(max_length=100)
 
 
+class SubjectAlias(models.Model):
+    '''Links a subject alias to its 'official' name.
+    Parsed data is not always accurate with subject names, so to solve
+    inconsistencies, this model will link those mispellings with the actual
+    subject.
+    '''
+    name = models.CharField(max_length=100)
+    subject = models.ForeignKey(Subject)
+
+    class Meta:
+        verbose_name_plural = 'Subject aliases'  # admin will show correct plural
+
+
 class Year(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
 
@@ -76,6 +89,6 @@ class DegreeSubject(models.Model):
     group = models.CharField(max_length=50, choices=GROUP_CHOICES)
 
 
-class Calendar:
-    calendar = models.FileField
+class Calendar(models.Model):
+    calendar = models.FileField(upload_to='.')
     degree_subject = models.ManyToManyField(DegreeSubject)

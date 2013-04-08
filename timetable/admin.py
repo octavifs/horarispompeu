26e86267898fed83d9eefa6502bffed9c50d4c9e
Octavi Font Sola <tavifont@gmail.com>
2013-04-08 13:12:43 +0200

    Much better admin view of both DegreeSubject and Calendar. Now it is visual enough so you can extract some useful information of what the system contains. Maybe I will add more search fields later on.

--- timetable/admin.py
+++ timetable/admin.py
@@ -15,6 +15,21 @@ class SubjectAliasAdmin(admin.ModelAdmin):
     list_display = ['name', 'subject']
 
 
+class DegreeSubjectAdmin(admin.ModelAdmin):
+    def subject_name(self, degreesubject):
+        return degreesubject.subject.name
+
+    def subject_faculty(self, degreesubject):
+        return degreesubject.subject.faculty
+
+    def degree_name(self, degreesubject):
+        return degreesubject.degree.name
+
+    search_fields = ['subject__name', 'degree__name']
+    list_display = ['subject_name', 'subject_faculty', 'degree_name', 'year',
+                    'term', 'group', 'academic_year']
+
+
 class LessonAdmin(admin.ModelAdmin):
     def subject_name(self, lesson):
         return lesson.subject.name
@@ -28,6 +43,21 @@ class LessonAdmin(admin.ModelAdmin):
     date_hierarchy = 'date_start'
     list_filter = ['date_start', 'kind', 'group']
 
+
+class CalendarAdmin(admin.ModelAdmin):
+    def degree_subjects_list(self, calendar):
+        subjects = map(
+            lambda entry: '{0} {1}'.format(*entry.values()),
+            calendar.degree_subjects.all()
+                .values('subject__name', 'group').distinct()
+        )
+        return "\n".join(subjects)
+
+    readonly_fields = ('degree_subjects_list',)
+    fields = ['name', 'file', 'degree_subjects_list']
+    list_display = ['name', 'file', 'degree_subjects_list']
+
+
 admin.site.register(Faculty)
 admin.site.register(Degree)
 admin.site.register(Subject, SubjectAdmin)
@@ -35,5 +65,5 @@ admin.site.register(SubjectDuplicate)
 admin.site.register(SubjectAlias, SubjectAliasAdmin)
 admin.site.register(AcademicYear)
 admin.site.register(Lesson, LessonAdmin)
-admin.site.register(DegreeSubject)
-admin.site.register(Calendar)
+admin.site.register(DegreeSubject, DegreeSubjectAdmin)
+admin.site.register(Calendar, CalendarAdmin)

c83bface6c2145727e29a62258cd79face6dfbe9
Octavi Font <tavifont@gmail.com>
2015-08-13 06:02:11 -0400

    manage.py update no longer tries to rebuild the calendar files

--- timetable/management/commands/update.py
+++ timetable/management/commands/update.py
@@ -1,19 +1,13 @@
 # encoding: utf-8
-import operator
-
 from django.core.management.base import NoArgsCommand
 from django.conf import settings
-from django.db.models import Q
-from django.core.files.base import ContentFile
 
 from timetable import scraper
-from timetable import ical
-from timetable.models import DegreeSubject, Calendar, Lesson
-from functools import reduce
+from timetable.models import DegreeSubject
 
 
 class Command(NoArgsCommand):
-    help = ("Update DB. Update lessons and rewrite calendars. Only selects "
+    help = ("Update DB. Update lessons. Only selects "
             "calendars from current academic year and term, if defined")
 
     def handle_noargs(self, **options):
@@ -28,18 +22,4 @@ class Command(NoArgsCommand):
         self.stdout.write("Updating lessons. This will take a while...\n")
         scraper.populate_lessons(degree_subjects)
         self.stdout.write("DONE!\n")
-        self.stdout.write("Updating calendars. This may also take a while...\n")
-        calendars = Calendar.objects.filter(degree_subjects__in=degree_subjects)
-        for calendar in calendars:
-            q_list = []
-            for ds in calendar.degree_subjects.all():
-                q = Q(
-                    subject=ds.subject,
-                    group_key=ds.group_key,
-                    academic_year=settings.ACADEMIC_YEAR)
-                q_list.append(q)
-            lessons_filter = reduce(operator.or_, q_list)
-            lessons = Lesson.objects.filter(lessons_filter)
-            calendar.file.save(calendar.name + '.ics',
-                ContentFile(ical.generate(lessons)))
-        self.stdout.write("DONE!\n")
+
